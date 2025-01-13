c0a85f735d3ec244c4d4f68c0cdfd8a898c96a64
cyril-hhf <cyrilhhf@gmail.com>
2025-01-14 01:11:30 +0800

    restructure main wrapper to enhance home page appearance

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -69,9 +69,10 @@ export default function Vans() {
     }
 
     return (
-        <div className="van-list-container">
-            <h1>Explore our van options</h1>
-            <div className="van-list-filter-buttons">
+        <main>
+            <div className="van-list-container">
+                <h1>Explore our van options</h1>
+                <div className="van-list-filter-buttons">
                 <button
                     onClick={() => handleFilterChange("type", "simple")}
                     className={
@@ -82,29 +83,29 @@ export default function Vans() {
                 <button
                     onClick={() => handleFilterChange("type", "luxury")}
                     className={
-                        `van-type luxury 
-                        ${typeFilter === "luxury" ? "selected" : ""}`
-                    }
-                >Luxury</button>
-                <button
-                    onClick={() => handleFilterChange("type", "rugged")}
-                    className={
-                        `van-type rugged 
-                        ${typeFilter === "rugged" ? "selected" : ""}`
-                    }
-                >Rugged</button>
-
-                {typeFilter ? (
+                                `van-type luxury 
+                                ${typeFilter === "luxury" ? "selected" : ""}`
+                            }
+                        >Luxury</button>
                     <button
-                        onClick={() => handleFilterChange("type", null)}
-                        className="van-type clear-filters"
-                    >Clear filter</button>
-                ) : null}
+                        onClick={() => handleFilterChange("type", "rugged")}
+                        className={
+                            `van-type rugged 
+                            ${typeFilter === "rugged" ? "selected" : ""}`
+                        }
+                    >Rugged</button>
 
+                    {typeFilter ? (
+                        <button
+                            onClick={() => handleFilterChange("type", null)}
+                            className="van-type clear-filters"
+                        >Clear filter</button>
+                    ) : null}
+                </div>
+                <div className="van-list">
+                    {vanElements}
+                </div>
             </div>
-            <div className="van-list">
-                {vanElements}
-            </div>
-        </div>
+        </main>
     )
 }
